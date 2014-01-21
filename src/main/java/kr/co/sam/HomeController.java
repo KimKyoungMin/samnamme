01b8c0efc36ede60dbd6eb72e2ac898cb5e074cc
kosta <kosta@kosta-HP>
2014-01-21 17:25:43 +0900

    tiles 적용

--- src/main/java/kr/co/sam/HomeController.java
+++ src/main/java/kr/co/sam/HomeController.java
@@ -17,23 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "home.htm", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "NewFile";
 	}
 	
+	@RequestMapping(value = "login.htm", method = RequestMethod.GET)
+	public String login(Model model){
+		return "loginForm";
+	}
+	
+	@RequestMapping(value = "join.htm", method = RequestMethod.GET)
+	public String join(Model model){
+		return "joinForm";
+	}
 }
