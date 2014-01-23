94ef902bd611eda951344786dd3930c7f5224a1f
Administrator <Administrator@MSDN-SPECIAL>
2014-01-23 17:40:18 +0900

    1. 작성자 : 김경민 2. 내용 : (1) 캘린더에 공연 title 불러오기 3. 비고 : jsp 파일 일부, dao,dto, controller 일부 변경

--- src/main/java/co/kr/samman/controllers/ConcertController.java
+++ src/main/java/co/kr/samman/controllers/ConcertController.java
@@ -3,7 +3,10 @@ package co.kr.samman.controllers;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.kr.samman.dao.ConcertDao;
 
 @Controller
 public class ConcertController {
@@ -13,9 +16,23 @@ public class ConcertController {
 	
 	//콘서트 정보 페이지
 	@RequestMapping("concertmain.htm")
-	public String conertdetail() {
-		
+	public String concert(Model model) {
+//		System.out.println("인터페이스의 구현을   Mapper가담당 : notices");
+//		NoticeDao noticeDao = sqlSession.getMapper(NoticeDao.class);
+//		
+//		///////////////////////////////////////////////////////////////			
+//		model.addAttribute("user",noticeDao.getUser());
+		System.out.println("ConcertController.concert()");
+		ConcertDao concertdao = sqlSession.getMapper(ConcertDao.class);
+		System.out.println("ConcertController.concert() get Mapper");
+		model.addAttribute("concertlist",concertdao.getConcertList());
+		System.out.println("ConcertController.concert() set Attribute");
 		return "concert.concertmain";
+	}
+	
+	@RequestMapping("concertdetail.htm")
+	public String concertdetail(){
+		return "concert.concertdetail";
 	}
 
 }
