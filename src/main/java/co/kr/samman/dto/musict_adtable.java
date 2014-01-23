5e24a649da959447b5da5b70974abd4d8e5f56e4
kosta <kosta@kosta-HP>
2014-01-23 19:24:15 +0900

    comit

---
+++ src/main/java/co/kr/samman/dto/musict_adtable.java
@@ -0,0 +1,92 @@
+package co.kr.samman.dto;
+
+import java.util.ArrayList;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class musict_adtable {
+	private int minfonum;
+	private String mtitle;
+	private String mfilename;
+	private String murl;
+	private String mpicname;
+	private String msname;
+	private String mgrade;
+	private String metcinfo;
+	
+	private String aid;
+	private String apwd;
+	
+	private ArrayList<MultipartFile> files;
+	
+	public ArrayList<MultipartFile> getFiles() {
+		return files;
+	}
+	public void setFiles(ArrayList<MultipartFile> files) {
+		this.files = files;
+	}
+	
+	public int getMinfonum() {
+		return minfonum;
+	}
+	public void setMinfonum(int minfonum) {
+		this.minfonum = minfonum;
+	}
+	public String getMtitle() {
+		return mtitle;
+	}
+	public void setMtitle(String mtitle) {
+		this.mtitle = mtitle;
+	}
+	public String getMfilename() {
+		return mfilename;
+	}
+	public void setMfilename(String mfilename) {
+		this.mfilename = mfilename;
+	}
+	public String getMurl() {
+		return murl;
+	}
+	public void setMurl(String murl) {
+		this.murl = murl;
+	}
+	public String getMpicname() {
+		return mpicname;
+	}
+	public void setMpicname(String mpicname) {
+		this.mpicname = mpicname;
+	}
+	public String getMsname() {
+		return msname;
+	}
+	public void setMsname(String msname) {
+		this.msname = msname;
+	}
+	public String getMgrade() {
+		return mgrade;
+	}
+	public void setMgrade(String mgrade) {
+		this.mgrade = mgrade;
+	}
+	public String getMetcinfo() {
+		return metcinfo;
+	}
+	public void setMetcinfo(String metcinfo) {
+		this.metcinfo = metcinfo;
+	}
+	
+	public String getAid() {
+		return aid;
+	}
+	public void setAid(String aid) {
+		this.aid = aid;
+	}
+	public String getApwd() {
+		return apwd;
+	}
+	public void setApwd(String apwd) {
+		this.apwd = apwd;
+	}
+	
+
+}
