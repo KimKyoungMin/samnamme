1a9ccb1bd3c7280a0128cde1ee6b2ccd96363722
KOSTA <KOSTA@KOSTA-HP>
2014-02-06 09:17:01 +0900

    비디오 메뉴 비동기처리 80% 처리완료
    작성자 = 김정후운

--- src/main/java/co/kr/samman/controllers/VideoController.java
+++ src/main/java/co/kr/samman/controllers/VideoController.java
@@ -1,8 +1,13 @@
 package co.kr.samman.controllers;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +15,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import co.kr.samman.dao.BoardDao;
 import co.kr.samman.dao.MusicDao;
+import co.kr.samman.dao.SettingDao;
 import co.kr.samman.dao.VideoDao;
+import co.kr.samman.dto.cont;
 import co.kr.samman.dto.musict;
+import co.kr.samman.dto.usert;
 
 @Controller
 public class VideoController {
@@ -54,10 +63,10 @@ public class VideoController {
 	
 	//뮤직 비디오 페이지
 		@RequestMapping(value="musicViedo.user" , method=RequestMethod.GET)
-		public String video(HttpServletRequest request, Model model, int Minfonum, int page) {
+		public String video(HttpServletRequest request, Model model, int Minfonum) {
 			
 			
-			page = 1;
+			int page = 1;
 			int limit = 6;
 			if(request.getParameter("page") != null){
 				page = Integer.parseInt(request.getParameter("page"));
@@ -87,6 +96,42 @@ public class VideoController {
 			
 			
 			return "video.videomain";
+		}
+		@RequestMapping(value="pagechangeof.user" ,produces="text/plain;charset=UTF-8")
+		@ResponseBody
+		public String pagechange(HttpServletRequest request,HttpServletResponse response, Model model,int pagenum) throws UnsupportedEncodingException {
+			int page = pagenum;
+			int limit = 6;
+			int startrow = ((page-1)*limit);
+			VideoDao videoDao = sqlSession.getMapper(VideoDao.class);
+			List<musict> videoList = videoDao.getvediolists(startrow, limit);
+			int listcount = videoDao.listcount();
+			//Ajax 한글처리를위한 추가구문
+			request.setCharacterEncoding("UTF-8");
+			response.setHeader("Content-Type", "text/html; charest=utf-8");
+			int maxpage = (int)((double)listcount/limit + 0.95);
+			int startpage = (((int)((double)page / 10 + 0.9)) -1)*10 + 1;
+			int endpage = startpage + 10 - 1;
+			if(endpage > maxpage){
+				endpage = maxpage;
+			}
+			model.addAttribute("videoList", videoList);
+			//확인 필요
+			request.setAttribute("page", page);
+			request.setAttribute("startpage", startpage);
+			request.setAttribute("endpage", endpage);
+			JSONArray data = new JSONArray();
+			for(musict b : videoList){
+				//Json 객체 생성
+				JSONObject obj = new JSONObject();
+				obj.put("mtitle",b.getMtitle());
+				obj.put("minfonum", b.getMinfonum());
+				obj.put("msname", b.getMsname());
+				//Json 리스트에 객체를 추가함
+				data.add(obj);
+			}
+			System.out.println(data.toString());
+			return data.toString();
 		}
 		
 	
