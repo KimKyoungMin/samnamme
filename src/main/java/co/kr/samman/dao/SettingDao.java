9bc6072a91c632e70a0d9effa60ac508fca39369
Administrator <Administrator@MSDN-SPECIAL>
2014-02-08 15:25:46 +0900

    1. 작성자 : 김경민 2. 내용 : 뮤직 업데이트 작업 Signed-off-by: Administrator <Administrator@MSDN-SPECIAL>

--- src/main/java/co/kr/samman/dao/SettingDao.java
+++ src/main/java/co/kr/samman/dao/SettingDao.java
@@ -11,5 +11,8 @@ public interface SettingDao {
 	public void musicInsert(musict_adtable ma); //음악 insert
 	public void concertInsert(concert con); //공연 insert
 	public void getConcertdelete(int id); //공연 delete
-	
+	public void musicUpdatenoFile(musict_adtable ma);
+	public void musicUpdatenoFileOne(musict_adtable ma);
+	public void musicUpdatenoFileTwo(musict_adtable ma);
+	public void musicUpdatenoFileAll(musict_adtable ma);
 }
